649187db77d5996764d89a95464b9b53362c5a1c
slowisfast2030 <slowisfast2030@163.com>
2023-03-04 15:04:32 +0800

    all is well

--- xxf.py
+++ xxf.py
@@ -1,5 +1,8 @@
 # 导入requests模块
 import requests
+import sys
+
+content = sys.argv[1]
 
 # 请求的url地址
 url = 'http://43.153.121.63:5000/ai_chat/'
@@ -10,12 +13,16 @@ headers = {"content-type":"application/json"}
 # payload 为传入的参数
 payload = {
     "account":"xiaoxiaofan",
-    "message":[{"role":"user","content":"写一段二叉树中序遍历脚本"}]
+    "message":[{"role":"user","content":content}]
 }
 
 # json形式，参数用json
 res = requests.post(url,json=payload,headers=headers)
 # print(res.text)
 
+
+print('===============================================================================')
+
 print(res.json()['data'])
 
+print('===============================================================================')
