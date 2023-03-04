87e1e63639c4f0f88f03c560436abd984b8a677a
slowisfast <slowisfast2030@163.com>
2023-03-04 20:10:55 +0800

    all is well

---
+++ file.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const stream = fs.createReadStream('file.txt');
+
+stream.on('data', function(chunk) {
+    const lines = chunk.toString().split('\n');
+    for(let i = 0; i < lines.length; i++){
+       console.log(i+1, lines[i]);
+}
+
+})
