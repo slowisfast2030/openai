bc1a47d3247e3f83c78d7cabbe252d4806f4e8e2
slowisfast <slowisfast2030@163.com>
2023-03-04 19:46:14 +0800

    all is well

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
 
-print(openai.api_key)
+openai.api_key = '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'
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
