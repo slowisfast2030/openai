da802e6308429ec1555ef3c083bc35ac624e38e9
slowisfast <slowisfast2030@163.com>
2023-03-05 00:32:54 +0800

    all is well

--- file.sh
+++ file.sh
@@ -2,6 +2,7 @@
 line_num=0
 while read line
 do
-    line_num=$(($line_num+1))
-    printf "Line %3d : %s\n" $line_num "$line"
+    #line_num=$(($line_num+1))
+    let "line_num=line_num+1"
+    printf "Line %3d : %s\n" $line_num $line
 done < "file.txt"
