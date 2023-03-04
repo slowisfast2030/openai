33c6414e65d8f01f6d18296eba681f58b0049a1b
slowisfast2030 <slowisfast2030@163.com>
2023-03-04 15:11:24 +0800

    all is well

--- hello_world.py
+++ hello_world.py
@@ -5,10 +5,7 @@ print(openai.api_key)
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
+        {"role": "user", "content": "写一个二叉树遍历脚本"}
     ]
 )
 
