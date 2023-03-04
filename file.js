d7ad313477e8f6cb904dd116078a055b9bb60d82
slowisfast <slowisfast2030@163.com>
2023-03-04 20:20:19 +0800

    all is well

--- file.js
+++ file.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 
-const stream = fs.createReadStream('file.txt');
+// const stream = fs.createReadStream('file.txt');
 
-stream.on('data', function(chunk) {
-    const lines = chunk.toString().split('\n');
-    for(let i = 0; i < lines.length; i++){
-       console.log(i+1, lines[i]);
-}
-})
+// stream.on('data', function(chunk) {
+//     const lines = chunk.toString().split('\n');
+//     for(let i = 0; i < lines.length; i++){
+//        console.log(i+1, lines[i]);
+// }
+// })
+
+
+fs.readFile('file.txt', 'utf-8', (err, data) => {
+    if (err) throw err;
+  
+    const lines = data.trim().split('\n');
+  
+    lines.forEach((line, index) => {
+      console.log(`Line ${index + 1}: ${line}`);
+    });
+
+  });
