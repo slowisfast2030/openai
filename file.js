5bc32633bf5d523720397b0b86d2b804f554b283
slowisfast <slowisfast2030@163.com>
2023-03-04 20:24:52 +0800

    all is well

--- file.js
+++ file.js
@@ -10,13 +10,28 @@ const fs = require('fs');
 // })
 
 
-fs.readFile('file.txt', 'utf-8', (err, data) => {
+// fs.readFile('file.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+  
+//     const lines = data.trim().split('\n');
+  
+//     lines.forEach((line, index) => {
+//       console.log(`Line ${index + 1}  ${line}`);
+//     });
+
+//   });
+
+// 您可以使用模板字符串来对齐输出。
+// 在模板字符串中，您可以使用占位符${}来插入变量的值。
+// 将${index + 1}放在固定宽度的字段中，
+// 例如${index + 1}，可以使每行的行号对齐。
+  fs.readFile('file.txt', 'utf-8', (err, data) => {
     if (err) throw err;
   
     const lines = data.trim().split('\n');
   
     lines.forEach((line, index) => {
-      console.log(`Line ${index + 1}: ${line}`);
+      console.log(`Line ${String(index + 1).padStart(3)}: ${line}`);
     });
-
-  });
+  
+  }); // 这段代码的输出格式已对齐
