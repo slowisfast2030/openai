8e6a7bc7ab2087600568770d9044968591bb1ddd
slowisfast2030 <slowisfast2030@163.com>
2023-03-06 09:33:17 +0800

    all is well

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
-openai.api_key = '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'
+openai.api_key = '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'
 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
