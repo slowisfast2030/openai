9712df36cdc6c06764b416b3dc94d5dd55dcedb3
slowisfast2030 <slowisfast2030@163.com>
2023-03-06 09:44:15 +0800

    all is well

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,13 @@
 # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
-openai.api_key = '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'
+openai.api_key = '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'
 
 response = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-        {"role": "user", "content": "写一个二叉树遍历脚本"}
+        {"role": "user", "content": "shell脚本读取文件，一行行输出。给出行号，行号右对齐。"}
     ]
 )
 
-print(response)
+#print(response)
 print(response['choices'][0]['message']['content'])
