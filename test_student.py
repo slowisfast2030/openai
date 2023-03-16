5adb5841514441fc82a9bdb5917254f4e29161be
slowisfast2030 <slowisfast2030@163.com>
2023-03-16 11:59:47 +0800

    all is well

--- test_student.py
+++ test_student.py
@@ -1,28 +1,48 @@
-# 导入unittest模块
+# 定义一个简单的类
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero")
+        return x / y
+
+
+# 编写单元测试
 import unittest
-# 导入被测类
-from student import Student
 
-# 定义一个继承自unittest.TestCase的测试类
-class TestStudent(unittest.TestCase):
-    # 定义一个以test开头的测试方法，用来测试Student类的初始化方法是否正确地赋值给实例属性
-    def test_init(self):
-        # 创建一个被测对象，并传入一组测试数据
-        s = Student("Alice", 18)
-        # 使用断言来比较实例属性和预期值是否相等
-        self.assertEqual(s.name, "Alice")
-        self.assertEqual(s.age, 18)
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        # 每个测试用例执行前都会执行该方法
+        self.calculator = Calculator()
 
-    # 定义另一个以test开头的测试方法，用来测试Student类的greet方法是否正确地返回问候语
-    def test_greet(self):
-        # 创建另一个被测对象，并传入另一组测试数据
-        s = Student("Bob", 20)
-        # 调用被测方法，并获取返回值
-        result = s.greet()
-        # 使用断言来比较返回值和预期值是否相等
-        self.assertEqual(result, "Hello, I am Bob, and I am 20 years old.")
+    def test_add(self):
+        self.assertEqual(self.calculator.add(2, 3), 5)
+        self.assertEqual(self.calculator.add(0, 0), 0)
+        self.assertEqual(self.calculator.add(-2, 3), 1)
 
-# 如果是直接运行这个文件，则执行以下代码
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(2, 3), -1)
+        self.assertEqual(self.calculator.subtract(0, 0), 0)
+        self.assertEqual(self.calculator.subtract(-2, 3), -5)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(2, 3), 6)
+        self.assertEqual(self.calculator.multiply(0, 0), 0)
+        self.assertEqual(self.calculator.multiply(-2, 3), -6)
+
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(6, 3), 2)
+        self.assertRaises(ValueError, self.calculator.divide, 4, 0)
+        self.assertAlmostEqual(self.calculator.divide(5, 3), 1.6666666666666667, delta=0.00001)
+
+
 if __name__ == '__main__':
-    # 调用unittest.main()方法来运行所有以test开头的测试方法，并输出结果报告
     unittest.main()
