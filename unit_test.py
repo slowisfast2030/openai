5f36737afeb1977b8c886ed7258e5e4671526e62
slowisfast2030 <slowisfast2030@163.com>
2023-03-16 10:46:49 +0800

    all is well

--- unit_test.py
+++ unit_test.py
@@ -1,21 +1,25 @@
 import unittest
 
 def multiply(a, b):
+    """Return the product of two numbers."""
     return a * b
 
 class TestMathFunctions(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(2+2, 4)
+        # Test addition of two positive integers
+        self.assertEqual(2 + 2, 4)
         
     def test_subtraction(self):
-        self.assertEqual(5-3, 2)
+        # Test subtraction of two positive integers
+        self.assertEqual(5 - 3, 2)
         
-    def test_multiplication(self): # renamed from 'test_multiply'
+    def test_multiplication(self):
+        # Test multiplication of two positive integers
         self.assertEqual(multiply(3, 4), 12)
         
     def test_negative_multiplication(self):
+        # Test multiplication of a negative and positive integer
         self.assertEqual(multiply(-3, 4), -12)
         
 if __name__ == '__main__':
     unittest.main()
-
