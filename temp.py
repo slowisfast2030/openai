2a3cd2e4f2e6e54152150e2e0ac3da1a3f61af65
slowisfast <slowisfast2030@163.com>
2023-03-31 00:12:06 +0800

    all is well

--- temp.py
+++ temp.py
@@ -1,20 +1,14 @@
+# 写一段基于python的代码，实现以下功能：
+# 输入一个数组，返回升序后的新数组。给出代码，以及测试用例。
+# 例如：输入[3,2,1]，返回[1,2,3]。
+# 请在下面写出代码，以及测试用例。
 
-def quicksort(arr):
-    # This function implements quick sort algorithm
-    if len(arr) <= 1:
-        return arr  # Already sorted list with only one element
+def sort_list(list):
+    return sorted(list)
 
-    # Select pivot element as middle index element 
-    pivot = arr[len(arr) // 2]
-
-    # Divide the arr into two sublists (left and right) smaller than and greater than pivot element respectively
-    left_arr = [x for x in arr if x < pivot]
-    middle_arr = [x for x in arr if x == pivot]
-    right_arr = [x for x in arr if x > pivot]
-
-    # Recursively apply quicksort to both sublists and concatenate the results
-    return quicksort(left_arr) + middle_arr + quicksort(right_arr)
+if __name__ == '__main__':
+    list = [3,2,1]
+    print(sort_list(list))
 
 
-# Example usage of quicksort function
-print(quicksort([3, 6, 8, 10, 1, 2, 1]))
+
